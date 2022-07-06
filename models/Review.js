39d321dbf253e03b5c636da948f69c0c62aefbaf
Nate Traugh <n.traugh@yahoo.com>
2022-07-06 11:10:29 -0400

    added model for Review file, created relationship between User and Review

---
+++ models/Review.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Review extends Model {}
+
+Review.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+        star: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        message: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        review_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "User",
+                key: "id"
+            }
+        }
+    }, 
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'review',
+    }
+)
--- models/index.js
+++ models/index.js
@@ -1,5 +1,13 @@
 const User = require('./User');
-
+const Review = require("./Review");
 // TODO: ADD RELATIONSHIPS
 
-module.exports = { User };
+User.hasMany(Review, {
+    foreignKey: "review_id"
+})
+
+Review.belongsTo(User, {
+    foreignKey: "review_id"
+})
+
+module.exports = { User, Review };
