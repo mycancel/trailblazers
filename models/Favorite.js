8fa6ca6398a713c37d2872b38fd19f59aa449e02
Hans Reichgelt <hreichgelt06@gmail.com>
2022-07-07 11:58:59 -0400

    adding post favorite and delete favorite, edits in models/favorites.js and index.js

--- models/Favorite.js
+++ models/Favorite.js
@@ -1,39 +1,36 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Favorite extends Model {}
 
 Favorite.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-          },
-        value: {
-            type: DataTypes.BOOLEAN,
-        },
-        park_code: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        Favorite_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "user",
-                key: "id"
-            }
-        }
-    }, 
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'favorite',
-    }
-)
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    park_code: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "favorite",
+  }
+);
 
 module.exports = Favorite;
