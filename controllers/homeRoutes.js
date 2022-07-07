e4210c0cd06784b6aca32883979d416b25bccd83
grilledcheeseplease <lacynunnally402@gmail.com>
2022-07-07 12:34:56 -0400

    forgot to save changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Farorite } = require('../models');
-const withAuth = require('../utils/auth');
-
-const serialize = (data) => JSON.parse(JSON.stringify(data));
 
 router.get('/', (req, res) => {
   res.render('index');
