1bd6ec7566d993d95ba37c03fce2b4675e7ef330
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-07-06 22:20:52 -0400

    created api functions in parkApi.js; added searchroutes.js and search.js; added route in controllers/index.js; created router.get method to render search results to the page; added formhandler in search.js to recieve form information and call router.get request from searchroutes.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const searchRoutes = require('./searchRoutes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/search', searchRoutes);
 
 module.exports = router;
