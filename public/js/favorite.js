4cf4e2f4a320d18f8b01295c66821832b079d106
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-07-07 13:53:14 -0400

    finished conditional in favCheckbox to delete favorite when unchecked

--- public/js/favorite.js
+++ public/js/favorite.js
@@ -17,14 +17,32 @@ const favCheckBox = async () => {
     });
 
     if (response.ok) {
-      console.log('Park favorited.')
+      console.log("Park favorited.");
     } else {
       alert(response.statusText);
     }
   } else {
-    console.log('Park was unable to be deleted. Need code here.')
+    // pathname = '/park/:code'
+    const pathname = window.location.pathname;
+    const parkCode = pathname.slice(6);
+    // TODO: Check if parkCode is correct
+    // console.log(parkCode)
+
+    const response = await fetch("/api/favorites", {
+      method: "DELETE",
+      body: JSON.stringify({ parkCode: parkCode }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      console.log("Park unfavorited.");
+    } else {
+      alert(response.statusText);
+    }
   }
-}
+};
 
 // TODO: queryselect input type=checkbox
 // document.querySelector('').addEventListener('change', favCheckBox)
