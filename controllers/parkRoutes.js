f69230e46a281cf330a64f56b45c1c170bf95d35
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-07-10 20:50:01 -0400

    added background color missing

--- controllers/parkRoutes.js
+++ controllers/parkRoutes.js
@@ -51,6 +51,8 @@ router.get("/:code", async (req, res) => {
     const hoursOfOp = data[0].operatingHours[0].standardHours;
     const imgURL = data[0].images[0].url;
 
+    console.log(imgURL);
+
     // returns park information
     const park = {
       name: name,
